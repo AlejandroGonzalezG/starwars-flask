00fc6cf547c3fadc7b3e830bb5b42dd94f559f6c
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-07-27 22:22:31 +0000

    avance star wars

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, json
 from flask_migrate import Migrate
-from models import db, Todo, Contact
+from models import db, User, Character, Planet, Film, Specie, Vehicle, Starship, Favorite_Character, Favorite_Planet, Favorite_Vehicle
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -11,50 +11,111 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database_dev.db'
 db.init_app(app)
 Migrate(app, db) # db init, db migrate, db upgrade
 
-@app.route('/api/todos', methods=['GET'])
-def get_todos():
-    todos = Todo.query.all()
-    todos = list(map(lambda todo: todo.serialize(), todos))
-    return jsonify(todos), 200
+@app.route('/api/people', methods=['GET'])
+def get_character():
+    characters = Character.query.all()
+    characters = list(map(lambda character: character.serialize(), characters))
+    return jsonify(characters), 200
 
-@app.route('/api/todos/<int:id>', methods=['GET'])
-def get_todos_by_id(id):
-    todo = Todo.query.get(id)
-    return jsonify(todo.serialize()), 200
+@app.route('/api/people/<int:people_id>', methods=['GET'])
+def get_characters_by_id(people_id):
+    character = Character.query.get(people_id)
+    return jsonify(character.serialize()), 200
 
-@app.route('/api/todos', methods=['POST'])
-def create_todos():
-
+@app.route('/api/people', methods=['POST'])
+def create_characters():
     data = request.get_json()
-    
-    todo = Todo()
-    todo.label = data['label']
-    todo.done = data['done']
+    character = Character()
+    character.name = data['name']
+    character.height = data['height']
+    character.mass = data['mass']
+    character.hair_color = data['hair_color']
+    character.skin_color = data['skin_color']
+    character.eye_color = data['eye_color']
+    character.birth_year = data['birth_year']
+    character.gender = data['gender']
 
-    db.session.add(todo) # INSERT INTO todos (label, done) VALUES ('My First Task', false);
+    db.session.add(character) # INSERT INTO todos (label, done) VALUES ('My First Task', false);
     db.session.commit() # Finaliza el query
 
-    return jsonify(todo.serialize()), 201
+    return jsonify(character.serialize()), 201
 
-@app.route('/api/todos/<int:id>', methods=['PUT'])
-def update_todos(id):
-    
-    
-    todo = Todo.query.get(id)
-    todo.label = request.json.get('label')
-    todo.done = request.json.get('done')
-
-    db.session.commit() # Finaliza el query
-
-    return jsonify(todo.serialize()), 200
-
-@app.route('/api/todos/<int:id>', methods=['DELETE'])
-def delete_todos_by_id(id):
-    todo = Todo.query.get(id)
-    db.session.delete(todo)
+@app.route('/api/people/<int:people_id>', methods=['DELETE'])
+def delete_characters_by_id(people_id):
+    character = Character.query.get(people_id)
+    db.session.delete(character)
     db.session.commit()
-    return jsonify({"msg": "Todo deleted"}), 200
+    return jsonify(character.serialize()), 200
+
+@app.route('/api/planets', methods=['GET'])
+def get_planet():
+    planets = Planet.query.all()
+    planets = list(map(lambda planet: planet.serialize(), planets))
+    return jsonify(planets), 200
+
+@app.route('/api/planets/<int:planet_id>', methods=['GET'])
+def get_planet_by_id(planet_id):
+    planet = Planet.query.get(planet_id)
+    return jsonify(planet.serialize()), 200
+
+@app.route('/api/planets', methods=['POST'])
+def create_planets():
+    data = request.get_json()
+    planet = Planet()
+    planet.name = data['name']
+    planet.rotation_period = data['rotation_period']
+    planet.orbital_period = data['orbital_period']
+    planet.diameter = data['diameter']
+    planet.climate = data['climate']
+    planet.gravity = data['gravity']
+    planet.terrain = data['terrain']
+    planet.surface_water = data['surface_water']
+    planet.population = data['population']
+
+    db.session.add(planet) # INSERT INTO todos (label, done) VALUES ('My First Task', false);
+    db.session.commit() # Finaliza el query
+
+    return jsonify(planet.serialize()), 201
+
+@app.route('/api/planets/<int:planet_id>', methods=['DELETE'])
+def delete_planets_by_id(planet_id):
+    planet = Planet.query.get(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify(planet.serialize()), 200
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize(), users))
+    return jsonify(users), 200
+
+@app.route('/api/users/<int:user_id>', methods=['GET'])
+def get_users_by_id(user_id):
+    user = User.query.get(user_id)
+    return jsonify(user.serialize()), 200
+
+@app.route('/api/users', methods=['POST'])
+def create_users():
+    data = request.get_json()
+    user = User()
+    user.email = data['email']
+    user.password = data['password']
+    user.name = data['name']
+    user.lastname = data['lastname']
+
+    db.session.add(user) # INSERT INTO todos (label, done) VALUES ('My First Task', false);
+    db.session.commit() # Finaliza el query
+
+    return jsonify(user.serialize()), 201
+
+@app.route('/api/users/<int:user_id>', methods=['DELETE'])
+def delete_users_by_id(user_id):
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify(user.serialize()), 200
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run()    
--- src/models.py
+++ src/models.py
@@ -1,29 +1,223 @@
+import os
+import sys
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Text, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 db = SQLAlchemy()
 
-class Todo(db.Model):
-    __tablename__ = 'todos'
+class User(db.Model):
+    __tablename__='Users'
     id = db.Column(db.Integer, primary_key=True)
-    label = db.Column(db.String(100), unique=True, nullable=False)
-    done = db.Column(db.Boolean(), default=False)
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "label": self.label,
-            "done": self.done
-        }
-
-
-class Contact(db.Model):
-    __tablename__ = 'contacts'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    phone = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80))
+    lastname = db.Column(db.String(80))
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            "phone": self.phone
+            "password": self.password,
+            "name": self.name,
+            "lastname": self.lastname
         }
+
+class Character(db.Model):
+    __tablename__='Characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    height = db.Column(db.Integer)		
+    mass = db.Column(db.Integer)		
+    hair_color = db.Column(db.String(80))	
+    skin_color = db.Column(db.String(80))		
+    eye_color = db.Column(db.String(80))		
+    birth_year = db.Column(db.String(80))		
+    gender = db.Column(db.String(80))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender
+        }
+
+class Planet(db.Model):
+    __tablename__='Planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))		
+    rotation_period = db.Column(db.Integer)		
+    orbital_period = db.Column(db.Integer)	
+    diameter = db.Column(db.Integer)		
+    climate = db.Column(db.String(80))		
+    gravity = db.Column(db.String(80))
+    terrain = db.Column(db.String(80))
+    surface_water = db.Column(db.Integer)		
+    population = db.Column(db.String(80))		
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "population": self.population,
+        }
+
+class Film(db.Model):
+    __tablename__='Films'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))		
+    episode_id = db.Column(db.Integer)		
+    opening_crawl = db.Column(db.Text(500))		
+    director = db.Column(db.String(80))		
+    producer = db.Column(db.String(80))		
+    release_date = db.Column(db.Date)		
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "episode_id": self.episode_id,
+            "opening_crawl": self.opening_crawl,
+            "director": self.director,
+            "producer": self.producer,
+            "release_date": self.release_date,
+        }
+
+class Specie(db.Model):
+    __tablename__='Species'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))		
+    classification = db.Column(db.String(80))		
+    average_height = db.Column(db.Integer)		
+    skin_colors = db.Column(db.String(80))		
+    hair_colors = db.Column(db.String(80))		
+    eye_colors = db.Column(db.String(80))		
+    average_lifespan = db.Column(db.Integer)		
+    language = db.Column(db.String(80))		
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "classification": self.classification,
+            "average_height": self.average_height,
+            "skin_color": self.skin_color,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "average_lifespan": self.average_lifespan,
+            "language": self.language,
+        }
+
+class Vehicle(db.Model):
+    __tablename__='Vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))		
+    model = db.Column(db.String(80))		
+    manufacturer = db.Column(db.String(80))		
+    cost_in_credits = db.Column(db.Integer)	
+    lenght = db.Column(db.Float)		
+    max_atmosphering_speed = db.Column(db.Integer)	
+    crew = db.Column(db.Integer)		
+    passengers = db.Column(db.Integer)
+    cargo_capacity = db.Column(db.Integer)
+    consumables = db.Column(db.String(80))		
+    vehicle_class = db.Column(db.String(80))		
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "lenght": self.lenght,
+            "max_atmosphering": self.max_atmosphering,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "vehicle_class": self.vehicle_class,
+        }
+
+class Starship(db.Model):
+    __tablename__='Starships'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))		
+    model = db.Column(db.String(80))		
+    manufacturer = db.Column(db.String(80))		
+    cost_in_credits = db.Column(db.Integer)		
+    lenght = db.Column(db.Integer)		
+    max_atmosphering_speed = db.Column(db.Integer)		
+    crew = db.Column(db.String(80))		
+    passengers = db.Column(db.Integer)		
+    cargo_capacity = db.Column(db.Integer)		
+    consumables = db.Column(db.String(80))		
+    hyperdrive_rating = db.Column(db.String(80))		
+    MGLT = db.Column(db.Integer)		
+    starship_class = db.Column(db.String(80))		
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "lenght": self.lenght,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "hyperdrive_rating": self.hyperdrive_rating,
+            "MGLT": self.MGLT,
+            "starship_class": self.starship_class,
+        }
+
+class Favorite_Character(db.Model):
+    __tablename__= 'Favorites Characters'
+    users_id = db.Column(db.Integer, db.ForeignKey("Users.id"), primary_key=True)
+    character_id = db.Column(db.Integer, db.ForeignKey("Characters.id"), primary_key=True)
+
+    def serialize(self):
+        return {
+            "users_id": self.users_id,
+            "people_id": self.people_id,
+        }
+
+class Favorite_Vehicle(db.Model):
+    __tablename__= 'Favorites Vehicles'
+    users_id = db.Column(db.Integer, db.ForeignKey("Users.id"))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey("Vehicles.id"), primary_key=True)
+
+    def serialize(self):
+        return {
+            "users_id": self.users_id,
+            "vehicle_id": self.vehicle_id,
+        }
+
+class Favorite_Planet(db.Model):
+    __tablename__= 'Favorites Planets'
+    users_id = db.Column(db.Integer, db.ForeignKey("Users.id"), primary_key=True)
+    planets_id = db.Column(db.Integer, db.ForeignKey("Planets.id"), primary_key=True)
+
+    def serialize(self):
+        return {
+            "users_id": self.users_id,
+            "planets_id": self.planets_id,
+        }
+
